f4a494f8499ca5eae369652e24e0b6d69d112e98
Concourse Trivadis <hello@pretius.com>
2022-03-08 14:54:56 +0000

    [ci skip] PL/SQL Formatting

--- src/db/packages_specs/pkg_employees.sql
+++ src/db/packages_specs/pkg_employees.sql
@@ -1,15 +1,21 @@
-begin  
-for i in 1..100 loop  
-if i in (1,5,7) then  
-dbms_output.put_line(' These are known prime numbers '||i); 
- end if;  if i not in (1,5,7) then  
-if mod(i,3)=0 or mod(i,6)=0 or mod(i,9)=0
- then  null;   
-elsif mod(i,2)=0 or mod(i,4)=0 or mod(i,8)=0 then  
-null;  elsif mod(i,5)=0 or mod(i,10)=0 then  null;   
-elsif mod(i,7)=0 then  null;  else 
- dbms_output.put_line(' Is this a prime number?? '||i);  
-end if;  
-end if;  
-end loop;  
-end;
+BEGIN
+  FOR i IN 1..100
+  LOOP
+    IF i IN (1, 5, 7) THEN
+      dbms_output.put_line(' These are known prime numbers ' || i);
+    END IF;
+    IF i NOT IN (1, 5, 7) THEN
+      IF MOD(i, 3) = 0 OR MOD(i, 6) = 0 OR MOD(i, 9) = 0 THEN
+        NULL;
+      ELSIF MOD(i, 2) = 0 OR MOD(i, 4) = 0 OR MOD(i, 8) = 0 THEN
+        NULL;
+      ELSIF MOD(i, 5) = 0 OR MOD(i, 10) = 0 THEN
+        NULL;
+      ELSIF MOD(i, 7) = 0 THEN
+        NULL;
+      ELSE
+        dbms_output.put_line(' Is this a prime number?? ' || i);
+      END IF;
+    END IF;
+  END LOOP;
+END;
