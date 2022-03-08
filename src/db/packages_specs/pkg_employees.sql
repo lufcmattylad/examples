7f245187506624e7e7d33c158a331d1f89c6dae7
lufcmattylad <lufcmattylad@gmail.com>
2022-03-08 15:48:01 +0100

    Update pkg_employees.sql

--- src/db/packages_specs/pkg_employees.sql
+++ src/db/packages_specs/pkg_employees.sql
@@ -1,21 +1,20 @@
-BEGIN
-  FOR i IN 1..100
-  LOOP
-    IF i IN (1, 5, 7) THEN
-      dbms_output.put_line(' These are known prime numbers ' || i);
-    END IF;
-    IF i NOT IN (1, 5, 7) THEN
-      IF MOD(i, 3) = 0 OR MOD(i, 6) = 0 OR MOD(i, 9) = 0 THEN
-        NULL;
-      ELSIF MOD(i, 2) = 0 OR MOD(i, 4) = 0 OR MOD(i, 8) = 0 THEN
-        NULL;
-      ELSIF MOD(i, 5) = 0 OR MOD(i, 10) = 0 THEN
-        NULL;
-      ELSIF MOD(i, 7) = 0 THEN
-        NULL;
-      ELSE
-        dbms_output.put_line(' Is this a prime number?? ' || i);
-      END IF;
-    END IF;
-  END LOOP;
-END;
+declare   
+num number;  
+ tot number:=0;   
+var1 number;   
+var2 number; 
+begin   num:=&num;   
+tmp:=num;   
+while tmp>0   
+loop   
+  var1:=tmp mod 10;     
+tot:= tot + (var1*var1*var1);     
+tmp:=tmp/10;   
+end loop;   
+if(tot==num)
+ then     
+dbms_output.put_line(num||' is armstrong no'); 
+  else   
+  dbms_output.put_line(num||' is not a armstrong no');   
+end if 
+end;
